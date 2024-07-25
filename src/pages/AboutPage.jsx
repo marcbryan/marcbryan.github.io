936c74a2f91c3e65dc6718b773339abf2604b440
marcbryan <marcboakyeflores98@gmail.com>
2024-07-25 14:49:38 +0200

    Repo añadido en AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import { useTranslation, Trans } from "react-i18next";
 import reactLogo from '../assets/react.svg';
 import viteLogo from '/vite.svg';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function AboutPage() {
   const { t } = useTranslation();
@@ -24,15 +25,21 @@ function AboutPage() {
       </p>
       <ul></ul>
       <p>{t("aboutPage_text3")}</p>
+      <h2>{t("aboutPage_repository")}</h2>
+      <p>{t("aboutPage_text4")}</p>
+      <div className="d-flex">
+        <GitHubIcon fontSize="large" sx={{ mr: 1 }} />
+        <a href="https://github.com/marcbryan/marcbryan.github.io" target="_blank" rel="noopener noreferrer" style={{ alignSelf: "center" }}>https://github.com/marcbryan/marcbryan.github.io</a>      
+      </div>
       <h2>{t("aboutPage_reasons")}</h2>
       <p>
-        <Trans i18nKey="aboutPage_text4" >Udemy course:<a href="https://www.udemy.com/course/react-js-inicia-en-el-mundo-de-los-frameworks-de-javascript/" target="_blank"/></Trans><br/>
+        <Trans i18nKey="aboutPage_text5" >Udemy course:<a href="https://www.udemy.com/course/react-js-inicia-en-el-mundo-de-los-frameworks-de-javascript/" target="_blank"/></Trans><br/>
       </p>
-      <p>{t("aboutPage_text5")}</p>
+      <p>{t("aboutPage_text6")}</p>
       <h2>{t("aboutPage_resources")}</h2>
       <ul></ul>
       <h2>{t("aboutPage_doubtsProblems")}</h2>
-      <p>{t("aboutPage_text6")}</p>
+      <p>{t("aboutPage_text7")}</p>
       <ul></ul>
     </main>
   );
